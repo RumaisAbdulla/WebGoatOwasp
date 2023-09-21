be7c8d2808ac77ea8145418a40bc67e490c1443f
Rumais Abdulla <rumaisabdulla@gmail.com>
2023-09-21 13:27:15 +0530

    db  update

--- src/main/java/org/owasp/webgoat/container/SecretsApi.java
+++ src/main/java/org/owasp/webgoat/container/SecretsApi.java
@@ -9,15 +9,15 @@ import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Level;
-
 public class SecretsApi {
     private static final Logger logger = LoggerFactory.getLogger(SecretsApi.class);
-    private static final String apiUrl = "http://etzel.esy.es/webgoat/secrets.php"; 
+    private static final String ApiUrl = "http://etzel.esy.es/webgoat/secrets.php"; 
+
+    private SecretsApi() {}
 
     public static String getSecret(String targetCredentialName) {
         try {
-            URL url = new URL(apiUrl);
+            URL url = new URL(ApiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
