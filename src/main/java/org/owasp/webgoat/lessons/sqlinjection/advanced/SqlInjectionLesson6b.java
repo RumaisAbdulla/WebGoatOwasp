c00f3e6e5c371cf43f0fd807a81a7c12329e24bc
Rumais Abdulla <rumaisabdulla@gmail.com>
2023-10-19 16:51:41 +0530

    A05: Security Mis Config - Debug codes removed

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionLesson6b.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionLesson6b.java
@@ -69,11 +69,11 @@ public class SqlInjectionLesson6b extends AssignmentEndpoint {
           password = results.getString("password");
         }
       } catch (SQLException sqle) {
-        sqle.printStackTrace();
+        
         // do nothing
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      
       // do nothing
     }
     return (password);
